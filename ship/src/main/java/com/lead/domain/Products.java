498107d9ab50b59496f9dea7283b28818995ac37
xixeo <xixeo0121@gmail.com>
2024-08-27 21:57:57 +0900

    Products

--- ship/src/main/java/com/lead/domain/Products.java
+++ ship/src/main/java/com/lead/domain/Products.java
@@ -1,19 +1,10 @@
 package com.lead.domain;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,8 +29,8 @@ public class Products {
     private String part1;
     private String part2;
 
-    @ManyToOne
-    private Supplier supplier;
+//    @ManyToOne
+//    private Supplier supplier;
 
     // Getters and Setters
 }
---
+++ ship/src/main/java/com/lead/persistence/ProductsRepo.java
@@ -0,0 +1,9 @@
+package com.lead.persistence;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lead.domain.Products;
+
+
+public interface ProductsRepo extends JpaRepository<Products, Long> {
+    // query 
+}
