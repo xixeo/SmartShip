39c28569c5125e87f9b1635483c2c890aaa6e9e9
waterkyeong <su5368@naver.com>
2024-09-02 09:26:49 +0900

    calendar css 수정

--- src/01/Schedule.js
+++ src/01/Schedule.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 
 export default function Schedule() {
   return (
