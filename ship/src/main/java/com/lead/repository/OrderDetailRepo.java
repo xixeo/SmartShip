547bd1a5aee7fb271d8e1324e3c721480900f24e
xixeo <xixeo0121@gmail.com>
2024-09-04 11:01:50 +0900

    중복 조회 제거 - @EntityGraph 사용

--- ship/src/main/java/com/lead/repository/OrderDetailRepo.java
+++ ship/src/main/java/com/lead/repository/OrderDetailRepo.java
@@ -1,13 +1,20 @@
 package com.lead.repository;
 
-import com.lead.entity.OrderDetail;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.lead.entity.OrderDetail;
 
 @Repository
 public interface OrderDetailRepo extends JpaRepository<OrderDetail, Integer> {
+	@EntityGraph(attributePaths = {"item", "item.supplier", "item.category2", "item.category2.category1"})
     List<OrderDetail> findByOrderOrderId(Integer orderId);
 
+	//OrderDetail 엔티티를 조회할 때, 연관된 Items 또는 Orders 엔티티를 참조할 수 있다. 각 엔티티마다 별도의 쿼리가 발생.
+	//예를 들어, OrderDetail에서 Items를 가져올 때마다 별도의 쿼리가 실행되어 Items가 여러 번 조회된다.
+	//그걸 방지하고자 @EntityGraph를 설정해준다 -> findByOrderId 메서드를 호출할 때 OrderDetail과 함께 item도 한 번에 로딩하게 되어 불필요한 추가 쿼리를 방지
+	//그것도 별로라면 sql문 써야한다..
 }
